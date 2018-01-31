ce0f58fc4fa377ce35a00ed0e125aa6581887424
Andy Garcia <andy89190@gmail.com>
2018-01-31 13:22:35 -0800

    Initial Commit

---
+++ OSMA Fix/Assets/Script/PlayerMovement/IsGrounded.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IsGrounded : MonoBehaviour {
+
+    private bool grounded;
+
+    private float xHalfSize;
+    private float yHalfSize;
+
+    private LayerMask groundLayer;
+
+    public bool Grounded
+    {
+        get { return grounded;}
+    }
+
+	// Use this for initialization
+	void Start () {
+        xHalfSize = transform.localScale.x / 2;
+        yHalfSize = transform.localScale.y / 2;
+
+        groundLayer = LayerMask.GetMask("Ground");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        grounded = isGrounded();
+	}
+
+    private bool isGrounded()
+    {
+        Vector2 overLapA = new Vector2(transform.position.x - xHalfSize, transform.position.y - yHalfSize);
+        Vector2 overLapB = new Vector2(transform.position.x + xHalfSize, transform.position.y - yHalfSize);
+
+        return Physics2D.OverlapArea(overLapA, overLapB, groundLayer);
+    }
+
+}
---
+++ OSMA Fix/Assets/Script/PlayerMovement/TransformPlayerMovement.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransformPlayerMovement : MonoBehaviour {
+
+    public float horizontalModifier;
+    public float verticalModifier;
+    public float verticalLerpModifier;
+
+    private float jump;
+
+    private Vector2 newPosition;
+
+    private IsGrounded groundCheck;
+    private Rigidbody2D rb2D;
+
+	// Use this for initialization
+	void Start () {
+
+        jump = 0.0f;
+        
+        newPosition = Vector2.zero;
+
+        groundCheck = GetComponent<IsGrounded>();
+        rb2D = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Debug.Log(verticalModifier * Time.fixedDeltaTime);
+
+        newPosition.x = HorizontalMovement();
+        newPosition.y = VerticalMovement();
+
+        transform.Translate(newPosition);
+	}
+
+    private float HorizontalMovement()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+
+        return horizontal * horizontalModifier * Time.deltaTime;
+    }
+
+    private float VerticalMovement()
+    {
+        if (Input.GetButtonDown("Vertical") && groundCheck.Grounded)
+            jump = verticalModifier * Time.deltaTime;
+        else if (groundCheck.Grounded)
+            jump = 0.0f;
+
+        jump = Mathf.Lerp(0.0f, jump, verticalLerpModifier);
+
+        return jump;
+    }
+}
